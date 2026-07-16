684fe947e3b429c93b4d8da62b56222d1d4c9e81
ATLASOZLTD <info@atlasoz.co.uk>
2026-07-16 23:40:04 +0100

    add Google Analytics tracking

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { GoogleAnalytics } from "@next/third-parties/google";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -35,9 +36,10 @@ export default function RootLayout({
       lang="tr"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
-      <body className="min-h-full flex-col">
-        {children}
-      </body>
+      <body className="min-h-full flex flex-col">
+  {children}
+  <GoogleAnalytics gaId="G-MS4M0RD65Q" />
+</body>
     </html>
   );
 }
